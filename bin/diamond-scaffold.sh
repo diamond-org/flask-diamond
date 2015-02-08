dadcb9412fac275eae4ddb8d57654b13cc73b640
Ian Dennis Miller <ian@iandennismiller.com>
2015-02-08 02:23:21 -0500

    scaffold a new diamong app

---
+++ bin/diamond-scaffold.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# usage:
+# run diamond-scaffold.sh in the target project directory
+
+# start with a fresh environment
+rm -rf /tmp/diamond-project /tmp/diamond-app
+
+# clone flask-diamond and the diamond skeletons
+git clone https://github.com/iandennismiller/diamond-project /tmp/diamond-project
+git clone https://github.com/iandennismiller/diamond-app /tmp/diamond-app
+
+# apply the two diamond skeletons
+mrbob -w /tmp/diamond-project/skel
+mrbob --config .mrbob.ini -w /tmp/diamond-app/skel
