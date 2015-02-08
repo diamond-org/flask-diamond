28825f8beae34f5d96fedcda5167588cc643e15b
Ian Dennis Miller <ian@iandennismiller.com>
2015-02-08 02:32:51 -0500

    repair manifest

--- bin/meta.sh
+++ bin/meta.sh
@@ -19,6 +19,7 @@ cd /tmp/flask-diamond
 git checkout setup.py
 git checkout flask_diamond/__init__.py
 git checkout flask_diamond/tests/test_models.py
+git checkout MANIFEST.in
 
 # remove the schema migration that adds the Individual model, which is specific to diamond-app
 rm flask_diamond/migrations/versions/13011baa608a_individual.py
