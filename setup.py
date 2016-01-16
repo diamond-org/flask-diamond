18355aee1432f6a07ad20343918349a5e5b86c19
Ian Dennis Miller <ian@iandennismiller.com>
2016-01-16 16:29:19 -0500

    full short description

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def grep(attrname):
 setup(
     version=grep('__version__'),
     name='Flask-Diamond',
-    description="Flask-Diamond is a batteries-included Flask framework.",
+    description="Flask-Diamond is a batteries-included Flask framework. Easily scaffold a working application with sensible defaults, then override the defaults to customize it for your goals.",
     packages=[
         "flask_diamond",
         "flask_diamond.models",
