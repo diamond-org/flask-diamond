a72560b7acb666af2e1131320790718140bbee9b
Ian Dennis Miller <ian@iandennismiller.com>
2017-11-29 20:24:37 -0500

    more robust handling of config

--- flask_diamond/facets/configuration.py
+++ flask_diamond/facets/configuration.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Flask-Diamond (c) Ian Dennis Miller
 
+import os
+import json
+
 
 def init_configuration(self):
     """
@@ -10,5 +13,8 @@ def init_configuration(self):
 
     ``SETTINGS`` must contain a filename that points to the configuration file.
     """
-
-    self.app.config.from_envvar('SETTINGS')
+    if "SETTINGS" in os.environ and os.path.isfile(os.environ["SETTINGS"]):
+        self.app.config.from_envvar('SETTINGS')
+    elif "SETTINGS_JSON" in os.environ:
+        h = json.loads(os.environ["SETTINGS_JSON"])
+        self.app.config.update(**h)
